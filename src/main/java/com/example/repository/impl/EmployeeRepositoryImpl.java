48e9ace577294daeaa94282f16bf66f832973e50
koti <nambulakoti999@gmail.com>
2024-10-26 18:15:48 +0530

    Logging added to project

--- src/main/java/com/example/repository/impl/EmployeeRepositoryImpl.java
+++ src/main/java/com/example/repository/impl/EmployeeRepositoryImpl.java
@@ -23,48 +23,94 @@ public class EmployeeRepositoryImpl implements EmployeeRepository {
 	private static final Logger logger =LoggerFactory.getLogger(EmployeeRepositoryImpl.class);
 	@Override
 	public int saveEmployee(Employee employee) {
-		String sql = "insert into employee(name,department,salary) values (?,?,?)";
-		
-		return template.update(sql,employee.getName(),employee.getDepartment(),employee.getSalary());
+		String sql = "insert into employee(name,department,salary) values (?,?,?)";	
+		logger.debug("Execute SQL Query {}: with values : name={},department={},salary={}",sql,employee.getName(),employee.getDepartment(),employee.getSalary());
+		try {
+			
+		 int rowsEffected = template.update(sql,employee.getName(),employee.getDepartment(),employee.getSalary());
+		 logger.info("saved employee :  {} , Rows Effected : {}",employee,rowsEffected);
+		 return rowsEffected;
+		}
+		catch (Exception e) {
+			logger.error("Error saving employee : {}",employee,e);
+			throw e;
+		}
 	}
 
 	@Override
 	public int updateEmployee(Employee employee) {
 		String sql = "update employee set name=?,department=?,salary=? where id=? ";
-		
-		return template.update(sql, employee.getName(),employee.getDepartment(),employee.getSalary(),employee.getId());
+		logger.debug("Execute Update Query {} :,to update name={},department={},salary={},with id : {}",sql,employee.getName(),employee.getDepartment(),employee.getSalary(),employee.getId());
+		try
+		{
+		int rowsEffected = template.update(sql, employee.getName(),employee.getDepartment(),employee.getSalary(),employee.getId());
+		logger.info("updating employee : {}, with id : {} successfull and effected rows : {}",employee,employee.getId(),rowsEffected);
+		return rowsEffected;
+		}
+		catch (Exception e) {
+			logger.error("Error while updating employee : {},with id : {} failed",employee,employee.getId(),e);
+			throw e;
+		}
 	}
 
 	@Override
 	public Employee findById(int id) {
 		String sql = "select * from employee where id=?";
-		return template.queryForObject(sql, new EmployeeRowMapper(), id);
+		logger.debug("Execute a query to get employee {} by id : {}",sql,id);
+		try {
+		Employee employee = template.queryForObject(sql, new EmployeeRowMapper(), id);
+		logger.info("Found employee : {},with id : {}",employee,id);
+		return employee;
+		}
+		catch (Exception e) {
+			logger.error("Error  while getting employee by id : {} failed",id,e);
+			throw e;
+		}
 	}
 
 	@Override
 	public int deleteById(int id) {
 		String sql = "delete from employee where id=?";
-		return template.update(sql, id);
+		logger.debug("Execute a Query to delete employee {} with id : {}",sql,id);
+		try {
+		int rowsEffected = template.update(sql, id);
+		logger.info("Deleting employee with id : {} successfull",id);
+		return rowsEffected;
+		}
+		catch (Exception e) {
+			logger.error("Error occured while deleting employee with id : {}",id,e);
+			throw e;
+		}
 	}
 
 	@Override
 	public List<Employee> findAll() {
 		String sql = "select * from employee";
-		return template.query(sql,new EmployeeRowMapper());
-	}
-	
-	static class EmployeeRowMapper implements RowMapper<Employee> {
-
-		@Override
-		public Employee mapRow(ResultSet rs, int rowNum) throws SQLException {
-			return new Employee(
-					rs.getInt("id"),
-					rs.getString("name"),
-					rs.getString("department"),
-					rs.getDouble("salary"));
+		logger.debug("Execute Query to fetch list of employees from database {}",sql);
+		try {
+		List<Employee> employees = template.query(sql,new EmployeeRowMapper());
+		logger.info("Found {} employees.",employees.size());
+		return employees;
 		}
+		catch (Exception e) {
+			logger.error("Error occured while fetching all employees",e);
+			throw e;
+		}
+	}
+		
+  static class EmployeeRowMapper implements RowMapper<Employee> {
 
-}
+      @Override
+      public Employee mapRow(ResultSet rs, int rowNum) throws SQLException {
+          Employee employee = new Employee(
+                  rs.getInt("id"),
+                  rs.getString("name"),
+                  rs.getString("department"),
+                  rs.getDouble("salary"));
+          logger.debug("Mapped Employee: {}", employee);
+          return employee;
+      }
+  }
 		
 	
 }
@@ -87,122 +133,5 @@ public class EmployeeRepositoryImpl implements EmployeeRepository {
 
 
 
-//package com.example.repository.impl;
-//
-//import java.sql.ResultSet;
-//import java.sql.SQLException;
-//import java.util.List;
-//
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.jdbc.core.JdbcTemplate;
-//import org.springframework.jdbc.core.RowMapper;
-//import org.springframework.dao.DataAccessException;
-//import org.springframework.stereotype.Repository;
-//
-//import com.example.model.Employee;
-//import com.example.repository.EmployeeRepository;
-//
-//@Repository
-//public class EmployeeRepositoryImpl implements EmployeeRepository {
-//
-//    @Autowired
-//    private JdbcTemplate template;
-//
-//
-//    private static final Logger logger = LoggerFactory.getLogger(EmployeeRepositoryImpl.class);
-//
-//    @Override
-//    public int saveEmployee(Employee employee) {
-//        String sql = "INSERT INTO employee(name, department, salary) VALUES (?, ?, ?)";
-//        logger.debug("Executing SQL: {} with values: name={}, department={}, salary={}", 
-//                      sql, employee.getName(), employee.getDepartment(), employee.getSalary());
-//
-//        try {
-//            int rowsAffected = template.update(sql, employee.getName(), employee.getDepartment(), employee.getSalary());
-//            logger.info("Saved employee: {}. Rows affected: {}", employee, rowsAffected);
-//            return rowsAffected;
-//        } catch (DataAccessException ex) {
-//            logger.error("Error saving employee: {}", employee, ex);
-//            throw ex;  // Let Spring handle the exception.
-//        }
-//    }
-//
-//    @Override
-//    public int updateEmployee(Employee employee) {
-//        String sql = "UPDATE employee SET name=?, department=?, salary=? WHERE id=?";
-//        logger.debug("Executing SQL: {} with values: name={}, department={}, salary={}, id={}", 
-//                      sql, employee.getName(), employee.getDepartment(), employee.getSalary(), employee.getId());
-//
-//        try {
-//            int rowsAffected = template.update(sql, employee.getName(), employee.getDepartment(), employee.getSalary(), employee.getId());
-//            logger.info("Updated employee: {}. Rows affected: {}", employee, rowsAffected);
-//            return rowsAffected;
-//        } catch (DataAccessException ex) {
-//            logger.error("Error updating employee: {}", employee, ex);
-//            throw ex;
-//        }
-//    }
-//
-//    @Override
-//    public Employee findById(int id) {
-//        String sql = "SELECT * FROM employee WHERE id=?";
-//        logger.debug("Executing SQL: {} with id={}", sql, id);
-//
-//        try {
-//            Employee employee = template.queryForObject(sql, new EmployeeRowMapper(), id);
-//            logger.info("Found employee with id {}: {}", id, employee);
-//            return employee;
-//        } catch (DataAccessException ex) {
-//            logger.error("Error finding employee with id={}", id, ex);
-//            throw ex;
-//        }
-//    }
-//
-//    @Override
-//    public int deleteById(int id) {
-//        String sql = "DELETE FROM employee WHERE id=?";
-//        logger.debug("Executing SQL: {} with id={}", sql, id);
-//
-//        try {
-//            int rowsAffected = template.update(sql, id);
-//            logger.info("Deleted employee with id {}. Rows affected: {}", id, rowsAffected);
-//            return rowsAffected;
-//        } catch (DataAccessException ex) {
-//            logger.error("Error deleting employee with id={}", id, ex);
-//            throw ex;
-//        }
-//    }
-//
-//    @Override
-//    public List<Employee> findAll() {
-//        String sql = "SELECT * FROM employee";
-//        logger.debug("Executing SQL: {}", sql);
-//
-//        try {
-//            List<Employee> employees = template.query(sql, new EmployeeRowMapper());
-//            logger.info("Found {} employees.", employees.size());
-//            return employees;
-//        } catch (DataAccessException ex) {
-//            logger.error("Error retrieving all employees", ex);
-//            throw ex;
-//        }
-//    }
-//
-//    // RowMapper to map ResultSet to Employee object
-//    static class EmployeeRowMapper implements RowMapper<Employee> {
-//
-//        @Override
-//        public Employee mapRow(ResultSet rs, int rowNum) throws SQLException {
-//            Employee employee = new Employee(
-//                    rs.getInt("id"),
-//                    rs.getString("name"),
-//                    rs.getString("department"),
-//                    rs.getDouble("salary"));
-//            logger.debug("Mapped Employee: {}", employee);
-//            return employee;
-//        }
-//    }
-//}
+
 
--- src/main/java/com/example/restcontroller/EmployeeRestController.java
+++ src/main/java/com/example/restcontroller/EmployeeRestController.java
@@ -2,6 +2,8 @@ package com.example.restcontroller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,39 +23,83 @@ public class EmployeeRestController {
 	@Autowired
 	private EmployeeRepository repo;
 	
+	private static final Logger logger = LoggerFactory.getLogger(EmployeeRestController.class);
+	
 	@PostMapping("/save")
 	public String saveEmployee(@RequestBody Employee employee)
 	{
+		logger.info("Recieved a request to save employee : {} ",employee);
+		try {
 		repo.saveEmployee(employee);
+		logger.info("Employee Added Successfully : {}",employee);
 		return "Employee Added Successfully";
+		}
+		catch (Exception e) {
+			logger.error("Error while saving employee : {} ",employee,e);
+			return "Failed Add Employee";
+		}
 	}
 	
 	@PutMapping("/update/{id}")
 	public String updateEmployee(@PathVariable int id,@RequestBody Employee employee)
 	{
 		employee.setId(id);
+		logger.info("Recieved a request to update employee by id : {}",id);
+		try {
 		repo.updateEmployee(employee);
+		logger.info("updating employee : {},with id : {} is successfull",employee,id);
 		return "Employee Updated Successfully";
+		}
+		catch (Exception e) {
+			logger.error("updating employee : {}, with id : {} failed",employee,id,e);
+			throw e;
+		}
 	}
 	
 	@GetMapping("/employee/{id}")
 	public Employee findById(@PathVariable int id)
 	{
-		return repo.findById(id);
+		logger.info("Request to get employee by id : {}",id);
+		try
+		{
+		Employee employee = repo.findById(id);
+		logger.info("Founded employee with id : {},{}",id,employee);
+		return employee;
+	}
+		catch (Exception e) {
+			logger.error("Error while getting employee with id : {}",id,e);
+			throw e;
+		}
 	}
 	
 	
 	@DeleteMapping("/{id}")
 	public String  deleteById(@PathVariable int id)
 	{
+		logger.info("Request to delete employee with id : {}",id);
+		try
+		{
 		repo.deleteById(id);
 		return "Employee Deleted Successfully";
+		}
+		catch (Exception e) {
+			logger.error("Error occured while deleting employee with id : {}",id,e);
+			throw e;
+		}
 	}
 	
 	@GetMapping("/employee")
 	public List<Employee> findAll()
 	{
+		logger.info("Recieved a request to get list of employees");
+		try {
 		return repo.findAll();
+		}
+		catch (Exception e) {
+			logger.error("Error occured while fetching list of employees",e);
+			throw e;
+		}
+		
 	}
 }
 
@@ -73,114 +119,5 @@ public class EmployeeRestController {
 
 
 
-//
-//
-//
-//
-//package com.example.restcontroller;
-//
-//import java.util.List;
-//
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.*;
-//
-//import com.example.model.Employee;
-//import com.example.repository.EmployeeRepository;
-//
-//@RestController
-//@RequestMapping("/api/employees") 
-//public class EmployeeRestController {
-//
-//    // Initialize Logger
-//    private static final Logger logger = LoggerFactory.getLogger(EmployeeRestController.class);
-//
-//    @Autowired
-//    private EmployeeRepository repo;
-//
-//    
-//    @PostMapping("/save")
-//    public String saveEmployee(@RequestBody Employee employee) {
-//        logger.info("Received request to save employee: {}", employee);
-//
-//        try {
-//            repo.saveEmployee(employee);
-//            logger.info("Employee saved successfully: {}", employee);
-//            return "Employee Added Successfully";
-//        } catch (Exception e) {
-//            logger.error("Error saving employee: {}", employee, e);
-//            return "Failed to Add Employee";
-//        }
-//    }
-//
-//    /**
-//     * Update an existing Employee by ID
-//     */
-//    @PutMapping("/update/{id}")
-//    public String updateEmployee(@PathVariable int id, @RequestBody Employee employee) {
-//        logger.info("Received request to update employee with id {}: {}", id, employee);
-//
-//        try {
-//            employee.setId(id);
-//            repo.updateEmployee(employee);
-//            logger.info("Employee updated successfully with id {}: {}", id, employee);
-//            return "Employee Updated Successfully";
-//        } catch (Exception e) {
-//            logger.error("Error updating employee with id {}: {}", id, e);
-//            return "Failed to Update Employee";
-//        }
-//    }
-//
-//    /**
-//     * Find Employee by ID
-//     */
-//    @GetMapping("/{id}")
-//    public Employee findById(@PathVariable int id) {
-//        logger.info("Received request to find employee with id {}", id);
-//
-//        try {
-//            Employee employee = repo.findById(id);
-//            logger.info("Found employee with id {}: {}", id, employee);
-//            return employee;
-//        } catch (Exception e) {
-//            logger.error("Error finding employee with id {}", id, e);
-//            throw e;  // Re-throw the exception to allow Spring to handle it
-//        }
-//    }
-//
-//    /**
-//     * Delete Employee by ID
-//     */
-//    @DeleteMapping("/{id}")
-//    public String deleteById(@PathVariable int id) {
-//        logger.info("Received request to delete employee with id {}", id);
-//
-//        try {
-//            repo.deleteById(id);
-//            logger.info("Employee deleted successfully with id {}", id);
-//            return "Employee Deleted Successfully";
-//        } catch (Exception e) {
-//            logger.error("Error deleting employee with id {}", id, e);
-//            return "Failed to Delete Employee";
-//        }
-//    }
-//
-//    /**
-//     * Find All Employees
-//     */
-//    @GetMapping
-//    public List<Employee> findAll() {
-//        logger.info("Received request to fetch all employees");
-//
-//        try {
-//            List<Employee> employees = repo.findAll();
-//            logger.info("Found {} employees", employees.size());
-//            return employees;
-//        } catch (Exception e) {
-//            logger.error("Error fetching all employees", e);
-//            throw e;
-//        }
-//    }
-//}
+
 
